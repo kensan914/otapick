f21580fc0ea27f284ba9d1bfe9ca80805a47a6cd
toriumi <ken.tennis914@gmail.com>
2019-12-10 23:44:47 +0900

    can use mysql

--- config/settings.py
+++ config/settings.py
@@ -81,12 +81,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'otapick',
+        'USER': 'root',
+        'PASSWORD': 'gin-TK46',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
