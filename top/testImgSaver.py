8db4d75f4725c761eda95546a0856e24a249b490
toriumi <ken.tennis914@gmail.com>
2020-02-06 00:04:56 +0900

    testes

--- top/testImgSaver.py
+++ top/testImgSaver.py
@@ -3,8 +3,8 @@ import requests
 
 
 def testImgSave():
-    os.makedirs('/var/www/otapick/media/testes', exist_ok=True)
-    img_file = open('/var/www/otapick/media/testes/test.jpg', 'wb')
+    os.makedirs('/var/www/otapick/media/blog_images/testes', exist_ok=True)
+    img_file = open('/var/www/otapick/media/blog_images/testes/test.jpg', 'wb')
 
     response = requests.get('https://cdn.keyakizaka46.com/files/14/diary/k46/member/moblog/201909/mobUWU5g7.jpg')
     image = response.content
