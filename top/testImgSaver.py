bebdc67a91b64dfe01c2d4aaa634230adfa25aea
toriumi <ken.tennis914@gmail.com>
2020-02-07 04:58:30 +0900

    testes

--- top/testImgSaver.py
+++ top/testImgSaver.py
@@ -91,6 +91,7 @@ def save_img(img_urls, group_id, blog_ct, writer_ct, progress):
 def testImgSave(blog_url, group_id, blog_ct, writer_ct):
     print('はじまったよーー')
     print('blog_ct=',blog_ct)
+    print('のid', Blog.objects.get(blog_ct=blog_ct).id)
 
     # progress = Progress.objects.get(target_id=1)
     if Progress.objects.filter(target_id=Blog.objects.get(blog_ct=blog_ct).id).exists():
