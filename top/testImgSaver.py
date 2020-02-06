0ebb9a8ae595c119bea16af5cbdc04cf0e208915
toriumi <ken.tennis914@gmail.com>
2020-02-07 04:00:20 +0900

    testes

--- top/testImgSaver.py
+++ top/testImgSaver.py
@@ -90,7 +90,8 @@ def testImgSave():
     group_id = 1
     blog_ct = 30958
     writer_ct = '12'
-    progress = Progress.objects.get(target_id=1)
+    # progress = Progress.objects.get(target_id=1)
+    progress = Progress.objects.create(target_id=100)
     # print(Blog.objects.filter(id=progress.target_id).exists())
     save_img(get_img_url(blog_url, group_id), group_id, blog_ct, writer_ct, progress)
 
