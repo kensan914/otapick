981210b8860ae2f6e33e7122c8fbfd3b2bfd32f2
kensan914 <52157596+kensan914@users.noreply.github.com>
2020-03-05 08:17:25 +0900

    PIL all load

--- search/scripts/blogRegister/compresser.py
+++ search/scripts/blogRegister/compresser.py
@@ -1,9 +1,12 @@
 from PIL import Image
+from PIL import ImageFile
 import os
 
 
 def compress_img(img_path):
     resize_width = 500
+
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
 
     img = Image.open(img_path)
     img_width, img_height = img.size
