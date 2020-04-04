23b9716bf236216a99f6925e56e2e6976d3e07e9
toriumi <ken.tennis914@gmail.com>
2020-04-04 20:04:22 +0900

    fix command

--- search/management/commands/calc_blog_score.py
+++ search/management/commands/calc_blog_score.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from download.models import Image
 from search.models import Blog
+from search.scripts.blogRegister import support
 
 
 class Command(BaseCommand):
@@ -8,10 +9,15 @@ class Command(BaseCommand):
            '3時間ごとに実行。'
 
     def handle(self, *args, **options):
-        blogs = Blog.objects.exclude(num_of_views=0)
-        for blog in blogs:
-            max_d1_per_week = Image.objects.filter(publisher=blog).order_by('-d1_per_week')[0].d1_per_week
-            score = blog.v1_per_week * 3 + blog.v2_per_week * 2 + blog.v3_per_week + max_d1_per_week * 3
-            blog.score = score
-            blog.save()
-        print(blogs)
+        try:
+            blogs = Blog.objects.exclude(num_of_views=0)
+            for blog in blogs:
+                max_d1_per_week = Image.objects.filter(publisher=blog).order_by('-d1_per_week')[0].d1_per_week
+                score = blog.v1_per_week * 3 + blog.v2_per_week * 2 + blog.v3_per_week + max_d1_per_week * 3
+                blog.score = score
+                blog.save()
+
+        except Exception as e:
+            support.print_console(e)
+        else:
+            support.print_console('finished calc_blog_score!!')
