160434a2825d693abc8d57548b9771db45ad8e2c
kensan914 <52157596+kensan914@users.noreply.github.com>
2020-04-05 00:50:22 +0900

    test calc_blog_score

--- search/management/commands/calc_blog_score.py
+++ search/management/commands/calc_blog_score.py
@@ -9,15 +9,22 @@ class Command(BaseCommand):
            '3時間ごとに実行。'
 
     def handle(self, *args, **options):
-        try:
-            blogs = Blog.objects.exclude(num_of_views=0)
-            for blog in blogs:
-                max_d1_per_week = Image.objects.filter(publisher=blog).order_by('-d1_per_week')[0].d1_per_week
-                score = blog.v1_per_week * 3 + blog.v2_per_week * 2 + blog.v3_per_week + max_d1_per_week * 3
-                blog.score = score
-                blog.save()
+        # try:
+        #     blogs = Blog.objects.exclude(num_of_views=0)
+        #     for blog in blogs:
+        #         max_d1_per_week = Image.objects.filter(publisher=blog).order_by('-d1_per_week')[0].d1_per_week
+        #         score = blog.v1_per_week * 3 + blog.v2_per_week * 2 + blog.v3_per_week + max_d1_per_week * 3
+        #         blog.score = score
+        #         blog.save()
+        #
+        # except Exception as e:
+        #     support.print_console(e)
+        # else:
+        #     support.print_console('finished calc_blog_score!!')
 
-        except Exception as e:
-            support.print_console(e)
-        else:
-            support.print_console('finished calc_blog_score!!')
+        blogs = Blog.objects.exclude(num_of_views=0)
+        for blog in blogs:
+            max_d1_per_week = Image.objects.filter(publisher=blog).order_by('-d1_per_week')[0].d1_per_week
+            score = blog.v1_per_week * 3 + blog.v2_per_week * 2 + blog.v3_per_week + max_d1_per_week * 3
+            blog.score = score
+            blog.save()
