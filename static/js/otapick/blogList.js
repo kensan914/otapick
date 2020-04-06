3e20b4059288253ba7d759a93e5ec71dd44ae515
toriumi <ken.tennis914@gmail.com>
2020-04-07 03:04:44 +0900

    add rewrite_card_text

--- static/js/otapick/blogList.js
+++ static/js/otapick/blogList.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
     $(".pagination").rPage();
     rewrite_card_parameter_num();
+    rewrite_card_writer_name();
+    // rewrite_card_blog_title();
 });
 
 $('[data-toggle="tooltip"]').tooltip();
 
 //InfiniteScroll
 var $grid = $('.grid').masonry({
-    itemSelector: '.grid .grid-item',
+    itemSelector: '.grid .grid-item'
 });
 
 var msnry = $grid.data('masonry');
@@ -17,11 +19,13 @@ $grid.infiniteScroll({
     append: '.grid .grid-item',
     outlayer: msnry,
     hideNav: '#to_next_page',
-    status: '.page-load-status',
+    status: '.page-load-status'
 });
 
 $grid.on('append.infiniteScroll', function () {
     twemoji.parse(document.body);
     $('[data-toggle="tooltip"]').tooltip();
     rewrite_card_parameter_num();
+    rewrite_card_writer_name();
+    // rewrite_card_blog_title();
 });
