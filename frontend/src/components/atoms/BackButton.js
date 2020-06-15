fe271a5504f950bb30a51cecfb3f4734769b9ced
kensan914 <kento.toriumi@gmail.com>
2020-06-16 03:58:15 +0900

    backButton

--- frontend/src/components/atoms/BackButton.js
+++ frontend/src/components/atoms/BackButton.js
@@ -3,13 +3,13 @@ import { withRouter } from 'react-router';
 
 
 class BackButton extends React.Component {
-  handleTo2Page = () => {
-    this.props.history.push('/react/2');
+  goBack = () => {
+    this.props.history.goBack();
   };
 
   render() {
     return (
-      <button onClick={this.handleTo2Page} className="btn btn-light rounded-circle p-0 otapick-back-button border shadow-sm back-from-download">
+      <button onClick={this.goBack} className="btn btn-light rounded-circle p-0 otapick-back-button border shadow-sm back-from-download">
         <i className="fas fa-arrow-left" style={{ color: "gray" }}></i>
       </button>
     );
