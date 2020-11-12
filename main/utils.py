e72acdf6603fd09752a21d2f2288a5258cb7557f
toriumi <ken.tennis914@gmail.com>
2020-11-13 07:42:45 +0900

    add django-admin-shortcuts

--- main/utils.py
+++ main/utils.py
@@ -3,4 +3,4 @@ from config import settings
 
 
 def geneMaintenanceMessage():
-    return 'メンテナンス中' if otapick.checkIsMaintaining(settings.BASE_DIR) else ''
+    return '実施中' if otapick.checkIsMaintaining(settings.BASE_DIR) else ''
