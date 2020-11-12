a43fc0ab7e35c7dc08ebb79fc2c95f374f6de319
toriumi <ken.tennis914@gmail.com>
2020-11-12 11:32:01 +0900

    update maintenance-mode settings

--- config/hosts.py
+++ config/hosts.py
@@ -5,5 +5,5 @@ host_patterns = patterns(
     '',
     host(r'', settings.ROOT_URLCONF, name=''),
     host(r'admin', 'admin.urls', name='admin'),
-    host(r'www', 'settings.ROOT_URLCONF', name='www'),
+    host(r'www', settings.ROOT_URLCONF, name='www'),
 )
