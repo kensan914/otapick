ec67e028b143a1e2dc38fcdb952f518f5ebacc5a
toriumi <ken.tennis914@gmail.com>
2020-11-12 12:35:01 +0900

    update OTP-45

--- config/hosts.py
+++ config/hosts.py
@@ -1,9 +1,11 @@
 from . import settings
 from django_hosts import patterns, host
+from admin import admin_urls
+
 
 host_patterns = patterns(
     '',
     host(r'', settings.ROOT_URLCONF, name=''),
-    host(r'admin', 'admin.urls', name='admin'),
+    host(r'admin', admin_urls, name='admin'),
     host(r'www', settings.ROOT_URLCONF, name='www'),
 )
