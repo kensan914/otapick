7f14456ad5e321fab60b5c2a64171fdbb984b227
toriumi <ken.tennis914@gmail.com>
2021-05-02 19:11:59 +0900

    update

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,7 +4,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DEBUG = process.env.NODE_ENV === "development";
 const OUTPUT_PUBLIC_PATH = `/static/${DEBUG ? "frontend-dev/" : "frontend/"}`;
 const OUTPUT_PATH = `${__dirname}/../static/${DEBUG ? "frontend-dev" : "frontend"}`;
-// const OUTPUT_PATH = `/var/www/otapick/static/${DEBUG ? "frontend-dev" : "frontend"}`;
 
 module.exports = {
   entry: "./src/index.jsx",
@@ -12,7 +11,6 @@ module.exports = {
     publicPath: OUTPUT_PUBLIC_PATH,
     path: OUTPUT_PATH,
     filename: "[name].bundle.js",
-    // filename: `[name].${process.env.NODE_ENV}.${DEBUG}.bundle.js`,
     chunkFilename: "[id].[contentHash].bundle.js", // contentHash: コードが変更されるたびに変更されるハッシュ値
   },
   resolve: {
